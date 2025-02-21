3884b87e25c01723009032714be274405d9fbde9
Enzo Bustos Da Silva <55506236+EnzoBustos@users.noreply.github.com>
2025-02-21 17:07:00 -0300

    Update quadrados-perfeitos.asm

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -1,44 +1,43 @@
-; ----- Calcula o quadrado de N
-@/100
-JP 034 ; AC = N
-ML 02C ; AC = AC*2
-AD 030 ; AC = AC+1
-AD 036 ; AC = AC + somatório
+@ /000
+    LD ZERO
+    MM /100
+    MM /090 ;guarda i
+    MM /092 ;guarda soma
+LP  SC SUB 
+    LD /090 
+    SB /080 ;checar se e 64
+    JZ /098 ;condicao de parada
+    LD /090
+    AD UM
+    MM /090
+    JP LP
+ 
+@ /020
+ZERO K /000
+UM K /001
+DOIS K /002
+ADRESS K /9100  
 
-;------ Salva o valor na memória e no somatório
-MM 102 ; mem[endereço] = AC
-MM 036 ; somatorio = AC
-
-; ----- Analisa se chegou ao final
-LD 034 ; AC = N
-SB 032 ; AC = AC - 63
-JZ 024 ; Se AC = 0, desvia para o fim
-
-; ----- Atualiza o endereço de escrita
-LD 02A ; AC = endereço
-AD 02C ; AC = AC + 2 (incrementa endereço)
-MM 02A ; atualiza o endereço
-AD 02E ; AC = Ac + 9000
-MM 008 ; Atualiza endereço de escrita
-
-; ----- Atualiza o valor de N
-LD 034 ; AC = N
-AD 030 ; AC = AC + 1 (incrementa N)
-MM 034 ; salva N
-
-     
-; ----- Recomeça o processo
-JP 000 ; Desvio para o início
-LV 000 ; AC = 0
-MM 100 ; mem[0100] = 0
-HM 0DE ; fim
-
-
-002A 0102 ; endereço
-JP 002 ; 2
-MM 000 ; 9000
-JP 001 ; 1
-JP 03F ; 63 (parada)
-
-JP 000 ; N
-JP 000 ; somatório
+@ /034
+SUB K /0
+    	LD /090
+    	ML DOIS
+    	AD UM
+    	AD /092
+    	MM /092
+    	LD ADRESS
+   	AD DOIS
+    	MM ADRESS
+    	MM SALVA
+    	LD /092
+SALVA   K /0000     
+    	RS SUB
+    
+    ;valor maximo = 0F81 (63^2)
+    
+@ /080
+ K /03E ;62 em hexadecimal
+    
+@ /098
+    HM /000
+    
